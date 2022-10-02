c7e4876e0403953d05564950afe226cfce99f7ab
kynikov <hellboynikov@gmail.com>
2022-10-02 20:21:12 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,192 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">    
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="/IMG/logo.png" alt=""/>
+          <div className="headerInfo">
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p className="opacity-5">Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img width={18} height={18} src="/IMG/card.svg" alt=""/>
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img width={18} height={18} src="/IMG/user.svg" alt=""/>
+          </li>
+        </ul>
       </header>
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1 className="">Все кроссовки</h1>
+          <div className="search-block">
+            <img src="/IMG/search.svg" alt="Search"/>
+            <input placeholder="Search..."/>
+          </div>
+        </div>
+        <div className="d-flex">
+        <div className="card">
+          <img width={133} height={112} src="/IMG/sneakers/01.jpg" alt=""/>
+          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена:</span>
+              <b>12 999 руб.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/IMG/plus.svg" alt=""/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={112} src="/IMG/sneakers/02.jpg" alt=""/>
+          <h5>Мужские Кроссовки Nike Air Max 270</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена:</span>
+              <b>12 999 руб.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/IMG/plus.svg" alt=""/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={112} src="/IMG/sneakers/03.jpg" alt=""/>
+          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена:</span>
+              <b>8 499 руб.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/IMG/plus.svg" alt=""/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={112} src="/IMG/sneakers/04.jpg" alt=""/>
+          <h5>Кроссовки Puma X Aka Boku Future Rider</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена:</span>
+              <b>12 999 руб.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/IMG/plus.svg" alt=""/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={112} src="/IMG/sneakers/05.jpg" alt=""/>
+          <h5>Мужские Кроссовки Under Armour Curry 8</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена:</span>
+              <b>12 999 руб.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/IMG/plus.svg" alt=""/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={112} src="/IMG/sneakers/06.jpg" alt=""/>
+          <h5>Мужские Кроссовки Nike Kyrie 7</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена:</span>
+              <b>12 999 руб.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/IMG/plus.svg" alt=""/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={112} src="/IMG/sneakers/07.jpg" alt=""/>
+          <h5>Мужские Кроссовки Jordan Air Jordan 11</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена:</span>
+              <b>12 999 руб.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/IMG/plus.svg" alt=""/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={112} src="/IMG/sneakers/08.jpg" alt=""/>
+          <h5>Мужские Кроссовки Nike LeBron XVIII</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена:</span>
+              <b>12 999 руб.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/IMG/plus.svg" alt=""/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={112} src="/IMG/sneakers/09.jpg" alt=""/>
+          <h5>Мужские Кроссовки Nike Lebron XVIII Low</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена:</span>
+              <b>12 999 руб.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/IMG/plus.svg" alt=""/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={112} src="/IMG/sneakers/10.jpg" alt=""/>
+          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена:</span>
+              <b>12 999 руб.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/IMG/plus.svg" alt=""/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={112} src="/IMG/sneakers/11.jpg" alt=""/>
+          <h5>Кроссовки Puma X Aka Boku Future Rider</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена:</span>
+              <b>12 999 руб.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/IMG/plus.svg" alt=""/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={112} src="/IMG/sneakers/12.jpg" alt=""/>
+          <h5>Мужские Кроссовки Nike Kyrie Flytrap IV</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена:</span>
+              <b>12 999 руб.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/IMG/plus.svg" alt=""/>
+            </button>
+          </div>
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
